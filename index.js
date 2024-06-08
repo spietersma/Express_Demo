54e3c19f6c6562da0254cba93355c7190c8d639d
Samuel <spietersma@gmail.com>
2024-06-08 14:42:53 -0400

    Added routes for update / solo get, paramaters in routing, etc

--- index.js
+++ index.js
@@ -9,32 +9,49 @@ app.get('/', (req, res) => {
 	res.send('Hello From Node Server!');
 });
 
-app.post('/api/products', async (req, res) => { 
-    try {
-        const product = await Product.create(req.body);
-        res.status(200).json(product);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
+// Add Product
+app.post('/api/products', async (req, res) => {
+	try {
+		const product = await Product.create(req.body);
+		res.status(200).json(product);
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
 });
 
+// Update Product
+
+app.put('/api/product/:id', async (req, res) => {
+	try {
+		const { id } = req.params;
+		const product = await Product.findByIdAndUpdate(id, req.body); //Could add new = true to update
+		if (!product) return res.status(404).json({ message: 'Product not found' });
+
+        const updatedProduct = await Product.findById(id);
+		res.status(200).json(updatedProduct);
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
+});
+
+//Get Product(s)
 app.get('/api/products', async (req, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json(products);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
+	try {
+		const products = await Product.find({});
+		res.status(200).json(products);
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
 });
 
-app.get('/api/products/:id', async (req, res) => {
-    try {
-        const {id} = req.params;
-        const product = await Product.findById(id);
-        res.status(200).json(product);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
+app.get('/api/product/:id', async (req, res) => {
+	try {
+		const { id } = req.params;
+		const product = await Product.findById(id);
+		res.status(200).json(product);
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
 });
 mongoose
 	.connect(
