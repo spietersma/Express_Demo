b5559d3fc8a4462d886905c4bb98f1ef723b52d9
Samuel <spietersma@gmail.com>
2024-06-08 13:56:40 -0400

    Added Mongo / Mongoose connectivity

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
 
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
-  });
 app.get('/', (req, res) => {
-  res.send('Hello From Node!');
+	res.send('Hello From Node Server!');
 });
 
+mongoose
+	.connect(
+		'mongodb+srv://spietersma:root@silverdb.tl6gmsu.mongodb.net/Node-API?retryWrites=true&w=majority&appName=SILVERDB'
+	)
+	.then(() => {
+		console.log('Connected to MongoDB');
+		app.listen(3000, () => {
+			console.log('Example app listening on port 3000!');
+		});
+	})
+	.catch((err) => {
+		console.log(err);
+	});
